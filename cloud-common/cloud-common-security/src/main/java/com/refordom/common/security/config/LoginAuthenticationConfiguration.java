0a9d3818f9567b3dcecd9fe0b8ee6ed8a8b5e609
pricess <544037232@qq.com>
2019-12-09 10:04:38 +0800

    优化工程结构

--- cloud-common/cloud-common-security/src/main/java/com/refordom/common/security/config/LoginAuthenticationConfiguration.java
+++ cloud-common/cloud-common-security/src/main/java/com/refordom/common/security/config/LoginAuthenticationConfiguration.java
@@ -3,7 +3,6 @@ package com.refordom.common.security.config;
 import com.refordom.common.security.constant.SecurityConstants;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -18,15 +17,11 @@ public class LoginAuthenticationConfiguration  {
     @Resource
     private AuthenticationFailureHandler securityAuthenticationFailHandler;
 
-    @Resource
-    private AuthenticationSuccessHandler loginFormAuthenticationSuccessHandler;
-
     public void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .loginPage(SecurityConstants.DEFAULT_AUTH_LOGIN_PAGE_ACCESS_TOKEN)
                 .loginProcessingUrl(SecurityConstants.DEFAULT_AUTH_LOGIN_FORM_ACCESS_TOKEN)
                 .failureHandler(securityAuthenticationFailHandler)
-                .successHandler(loginFormAuthenticationSuccessHandler)
            ;
     }
 }
