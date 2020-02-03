110569496170fef6a38c2970563340c824bc671d
pricess <>
2020-02-03 15:34:22 +0800

    重构核心代码

--- cloud-common/cloud-common-action-builder/src/main/java/com/refordom/common/action/builder/core/ServeRequest.java
+++ cloud-common/cloud-common-action-builder/src/main/java/com/refordom/common/action/builder/core/ServeRequest.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 import javax.servlet.Filter;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 
 /**
